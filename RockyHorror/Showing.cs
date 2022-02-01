cbd02c84cf85d4803f6b36ccda8f23e6616f8fca
Karionis <niklas.kanthak@online.de>
2022-02-01 22:56:29 +0100

    Add ShowTimes property

--- RockyHorror/Showing.cs
+++ RockyHorror/Showing.cs
@@ -10,5 +10,6 @@ namespace RockyHorror
     {
         public string Location { get; set; }
         public int  NumberOfSeats { get; set; }
+        public List<DateTime> ShowTimes { get; set; }
     }
 }
