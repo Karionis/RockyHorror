66a9f64c3948bda278f9f28c539de1595f500e1f
Karionis <niklas.kanthak@online.de>
2022-02-01 23:01:42 +0100

    Instantiate a showing (interactive rebased)

--- RockyHorror/Program.cs
+++ RockyHorror/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RockyHorror
 {
@@ -6,7 +7,18 @@ namespace RockyHorror
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var showing = new Showing();
+            showing.Location = "Brattle";
+            showing.NumberOfSeats = 250;
+            showing.ShowTimes = new List<DateTime>
+            {
+                new DateTime(0,0,0,10,0,0),
+                new DateTime(0,0,0,13,0,0),
+                new DateTime(0,0,0,16,0,0),
+                new DateTime(0,0,0,19,0,0),
+                new DateTime(0,0,0,22,0,0),
+                new DateTime(0,0,0,0,0,1)
+            };
         }
     }
 }
